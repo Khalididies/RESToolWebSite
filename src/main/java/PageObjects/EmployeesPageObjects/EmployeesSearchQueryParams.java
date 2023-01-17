2cc20d2466ec0111bf56d875e83dac8ba2996115
lenovo khalid <lenovo khalid>
2023-01-18 01:29:36 +0200

    Adding PageObjects Adding TestCases API and UI Adding APIObjectsModel Adding Utils Adding Entities

--- src/main/java/PageObjects/EmployeesPageObjects/EmployeesSearchQueryParams.java
+++ src/main/java/PageObjects/EmployeesPageObjects/EmployeesSearchQueryParams.java
@@ -8,25 +8,17 @@ import org.openqa.selenium.WebElement;
 public class EmployeesSearchQueryParams{
     WebDriver driver;
     WebElement Search;
-    WebElement Page;
-    WebElement Limit;
     WebElement Submit;
 
     public EmployeesSearchQueryParams(WebDriver driver) {
         this.driver=driver;
         Search = driver.findElement(By.xpath(EmployeesXpath.SearchQueryParamsSearchXpath));
-//        Page = driver.findElement(By.xpath(EmployeesXpath.SearchQueryParamsPageXpath));
-//        Limit = driver.findElement(By.xpath(EmployeesXpath.SearchQueryParamsLimitXpath));
         Submit = driver.findElement(By.xpath(EmployeesXpath.SearchQueryParamsSubmitXpath));
     }
 
     public void fillInSearch(String Name) {
         Search.clear();
         Search.sendKeys(Name);
-//        Page.clear();
-//        Page.sendKeys("1");
-//        Limit.clear();
-//        Limit.sendKeys("3");
     }
 
     public void clickSubmit() {
